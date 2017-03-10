039187e6a1cad9b1953d24b2c02cd139d422f57e
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-03-10 19:11:33 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='open-korean-text-python',
+    version='0.1.0',
+    description='Python interface to Open Korean Text Processor',
+    url='https://github.com/hyeon0145/open-korean-text-python',
+    author='Jonghwan Hyeon',
+    author_email='hyeon0145@gmail.com',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    keywords='open-korean-text open-korean-text-processor',
+    packages=find_packages(),
+    install_requires=['Jpype1'],
+)
