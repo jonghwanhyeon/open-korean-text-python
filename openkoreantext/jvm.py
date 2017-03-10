f6785e530831c134fd776ae61c3387555fe035a2
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-03-11 04:06:15 +0900

    Add all jar files to class path

--- openkoreantext/jvm.py
+++ openkoreantext/jvm.py
@@ -8,19 +8,11 @@ from jpype import *
 def clean_up():
     shutdownJVM()
 
-class_paths = [
-    'jar/scala-library-2.12.1.jar',
-    'jar/twitter-text-1.14.3.jar',
-    'jar/open-korean-text-1.1.jar',
-]
-
 path_prefix = os.path.dirname(os.path.realpath(__file__))
 class_paths = glob(os.path.join(path_prefix, 'jar', '*.jar'))
 
 parameters = [
-    '-Djava.class.path={}'.format(
-        os.pathsep.join(class_paths)
-    ),
+    '-Djava.class.path={}'.format(os.pathsep.join(class_paths)),
     '-ea',
 ]
 
